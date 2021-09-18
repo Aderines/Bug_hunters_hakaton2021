6a31839dfcfa1be78385e274587e5178a6db4642
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-09-18 16:59:29 +0300

    Тесты для примера, открытие страницы в фикстуре и "--ignore-certificate-errors"

--- fixture/home_page.py
+++ fixture/home_page.py
@@ -7,3 +7,19 @@ class HomeHelper:
 
 
 # Здесь можно начинать писать функции для тестирования страницы
+
+    def open_start_page(self):
+        self.wd = self.app.wd
+        if self.wd.current_url.endswith("/en") is not True:
+            self.wd.find_element_by_css_selector(".js-mobile-logo").click()
+            self.wd.find_elements_by_css_selector(".page-homepage.pageType-ContentPage")
+
+    def click_on_banners(self, name):
+        self.wd.find_element_by_css_selector(f".js-responsive-image[title='{name}']")
+        self.wd.find_element_by_css_selector(f".js-responsive-image[title='{name}']").click()
+        self.wd.find_element_by_css_selector(".product__list--wrapper")
+
+    def get_text_cataloge_name(self):
+        self.wd.find_element_by_css_selector(".breadcrumb .active")
+        return self.wd.find_element_by_css_selector(".breadcrumb .active").text
+
